6221b9f959799708c3c9cefac30bc435c63c6029
Jason Reich <jason.reich@anaplan.com>
2021-12-01 14:13:29 +0000

    Part one

--- src/day1.rs
+++ src/day1.rs
@@ -1,12 +1,12 @@
-pub fn count_increases(input: Vec<i32>) -> i32 {
+pub fn count_increases(input: impl Iterator<Item = i32>) -> i32 {
   let mut count: i32 = 0;
   let mut last: i32 = i32::MAX;
 
-  for value in input.iter() {
-    if *value > last {
+  for value in input {
+    if value > last {
       count += 1
     }
-    last = *value
+    last = value
   }
   
   count
@@ -14,11 +14,21 @@ pub fn count_increases(input: Vec<i32>) -> i32 {
 
 #[cfg(test)]
 mod test {
+  use super::count_increases;
+  use super::super::util::read_file;
+  
   #[test]
   fn example() {
     let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
     let output = 7;
 
-    assert_eq!(output, super::count_increases(input))
+    assert_eq!(output, count_increases(input.into_iter()))
+  }
+
+  #[test]
+  fn part1() {
+    let i = read_file("day1.txt").unwrap();
+    let count = count_increases(i);
+    println!("Count: {}", count);
   }
 }
