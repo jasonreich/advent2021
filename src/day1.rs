2bbfc83aaefc3acbab1329a66599f24056d4cdce
Jason Reich <jason.reich@anaplan.com>
2021-12-01 14:45:54 +0000

    Day one done

--- src/day1.rs
+++ src/day1.rs
@@ -12,9 +12,28 @@ pub fn count_increases(input: impl Iterator<Item = i32>) -> i32 {
   count
 }
 
+pub fn count_increases2(input: impl Iterator<Item = i32>) -> i32 {
+  let mut oldest = 0;
+  let mut middle = 0;
+  let mut newest = 0;
+
+  let windowed = input.enumerate().filter_map(| (index, input) | {
+    oldest = middle;
+    middle = newest;
+    newest = input;
+    if index >= 2 {
+      Some(oldest + middle + newest)
+    } else {
+      None
+    }
+  });
+  
+  count_increases(windowed)
+}
+
 #[cfg(test)]
 mod test {
-  use super::count_increases;
+  use super::*;
   use super::super::util::read_file;
   
   #[test]
@@ -29,6 +48,21 @@ mod test {
   fn part1() {
     let i = read_file("day1.txt").unwrap();
     let count = count_increases(i);
+    println!("Count: {}", count);
+  }
+
+  #[test]
+  fn example2() {
+    let input = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+    let output = 5;
+
+    assert_eq!(output, count_increases2(input.into_iter()))
+  }
+
+  #[test]
+  fn part2() {
+    let i = read_file("day1.txt").unwrap();
+    let count = count_increases2(i);
     println!("Count: {}", count);
   }
 }
