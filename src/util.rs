9e3a4f0336d177e05e36ebe63f1aec3190accff0
Jason Reich <jason@swiftorific.co.uk>
2021-12-02 07:50:12 +0000

    Try generifying the line reader

--- src/util.rs
+++ src/util.rs
@@ -3,18 +3,22 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::{BufReader, Lines, Result};
 
-pub fn read_file_as_i32(path: &str) -> Result<impl Iterator<Item = i32>> {
+pub fn read_file<T>(path: &str, line_parser: fn (String) -> Option<T>) -> Result<impl Iterator<Item = T>> {
   let mut full_path = current_dir()?;
   full_path.push("fixtures");
   full_path.push(path);
   let file = File::open(full_path)?;
   let lines_of_file: Lines<BufReader<File>> = BufReader::new(file).lines();
   let lines_as_parsed = lines_of_file
-    .filter_map(| maybe_line | {
+    .filter_map(move | maybe_line | {
       let line = maybe_line.ok()?;
-      line.parse().ok()
+      line_parser(line)
     });
   Ok(lines_as_parsed)
+}
+
+pub fn read_file_as_i32(path: &str) -> Result<impl Iterator<Item = i32>> {
+  read_file(path, |line| line.parse().ok())
 }
 
 #[cfg(test)]
