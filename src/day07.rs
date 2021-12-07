6e5f0e8dd43ac10884376a95c53e1e91289fee67
Jason Reich <jason.reich@anaplan.com>
2021-12-07 10:11:43 +0000

    Was there any point in passing by reference

--- src/day07.rs
+++ src/day07.rs
@@ -1,6 +1,6 @@
 type Crabs = Vec<i32>;
 
-pub fn part1(input: &Crabs) -> i32 {
+pub fn part1(input: Crabs) -> i32 {
   let lowest = input.iter().min().unwrap();
   let highest = input.iter().max().unwrap();
 
@@ -9,7 +9,7 @@ pub fn part1(input: &Crabs) -> i32 {
   ).min().unwrap()
 }
 
-pub fn part2(input: &Crabs) -> i32 {
+pub fn part2(input: Crabs) -> i32 {
   let lowest = input.iter().min().unwrap();
   let highest = input.iter().max().unwrap();
 
@@ -30,24 +30,24 @@ mod test {
   #[test]
   fn example_day07_part1() {
     let example_input: Crabs = vec![16,1,2,0,4,2,7,1,2,14];
-    assert_eq!(37, part1(&example_input));
+    assert_eq!(37, part1(example_input));
   }
 
   #[test]
   fn exec_day07_part1() {
     let input: Crabs = read_line("day07.txt");
-    println!("Day 07 Part 1: {}", part1(&input));
+    println!("Day 07 Part 1: {}", part1(input));
   }
 
   #[test]
   fn example_day07_part2() {
     let example_input: Crabs = vec![16,1,2,0,4,2,7,1,2,14];
-    assert_eq!(168, part2(&example_input));
+    assert_eq!(168, part2(example_input));
   }
 
   #[test]
   fn exec_day07_part2() {
     let input: Crabs = read_line("day07.txt");
-    println!("Day 07 Part 2: {}", part2(&input));
+    println!("Day 07 Part 2: {}", part2(input));
   }
 }
