60aa2a3d50c5f9003292f29cdb79e99338022d7d
Jason Reich <jason.reich@anaplan.com>
2021-12-09 07:41:56 +0000

    Parser

--- src/day09.rs
+++ src/day09.rs
@@ -1,4 +1,19 @@
+use crate::util::read_lines;
+
+pub fn parse_puzzle(file: &str) -> Vec<Vec<u32>> {
+  read_lines(file, |line: String| -> Option<Vec<u32>> {
+    Some(line.chars().map(|c| c.to_digit(10).unwrap()).collect())
+  }).unwrap().collect()
+}
+
 #[cfg(test)]
 mod test {
-  
+  use super::*;
+
+  #[test]
+  fn test_parse_puzzle() {
+    let input = parse_puzzle("day09.example");
+    assert_eq!(5, input.len());
+    assert_eq!(10, input[0].len());
+  }
 }
