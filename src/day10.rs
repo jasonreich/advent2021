7e82d8df17be497a73a09ffca15d775b3622267a
Jason Reich <jason.reich@anaplan.com>
2021-12-10 07:55:32 +0000

    Start day 10

---
+++ src/day10.rs
@@ -0,0 +1,38 @@
+
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  // #[test]
+  // fn test_parse_puzzle() {
+  //   let input = parse_puzzle("day10.example");
+  //   assert_eq!(5, input.len());
+  //   assert_eq!(10, input[0].len());
+  // }
+
+  // #[test]
+  // fn example_day10_part1() {
+  //   let input = parse_puzzle("day10.example");
+  //   assert_eq!(15, part1(input));
+  // }
+
+  // #[test]
+  // fn exec_day10_part1() {
+  //   let input = parse_puzzle("day10.txt");
+  //   println!("Day 10 Part 1 - {}", part1(input));
+  // }
+  
+  // #[test]
+  // fn example_day10_part2() {
+  //   let input = parse_puzzle("day10.example");
+  //   assert_eq!(1134, part2(input));
+  // }
+
+  // #[test]
+  // fn exec_day10_part2() {
+  //   let input = parse_puzzle("day10.txt");
+  //   println!("Day 10 Part 2 - {}", part2(input));
+  // }
+  
+}
