9f7804a67de2975ff3e75289bc8fc89735528719
Jason Reich <jason@swiftorific.co.uk>
2021-12-12 08:24:48 +0000

    Not working yet

--- src/day12.rs
+++ src/day12.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use multimap::MultiMap;
 use std::iter::FromIterator;
 
@@ -34,6 +35,32 @@ pub fn part1(input: Puzzle) -> u32 {
                 next.unwrap()
                     .iter()
                     .filter(|cave| is_big(cave) || path.iter().all(|c| c != cave))
+                    .for_each(|cave| {
+                        let mut new_cave = path.clone();
+                        new_cave.insert(0, cave);
+                        paths.push(new_cave)
+                    });
+            }
+        } else {
+            count += 1;
+        }
+    }
+    count
+}
+
+pub fn part2(input: Puzzle) -> u32 {
+    let mut count = 0;
+    let mut paths = vec![vec!["start"]];
+    while let Some(path) = paths.pop() {
+        let end = path[0];
+        if end != "end" {
+            let used_small = path.iter().filter(|&&cave| !is_big(&cave.to_string())).duplicates().count() > 0;
+            let next = input.get_vec(end);
+            if next.is_some() {
+                next.unwrap()
+                    .iter()
+                    .filter(|cave| *cave != "small")
+                    .filter(|cave| is_big(cave) || !used_small || path.iter().all(|c| c != cave))
                     .for_each(|cave| {
                         let mut new_cave = path.clone();
                         new_cave.insert(0, cave);
@@ -85,5 +112,29 @@ mod test {
     fn exec_day12_part1() {
         let input = read_puzzle("day12.txt");
         println!("Day 12 Part 1 - {}", part1(input));
+    }
+
+    #[test]
+    fn example1_day12_part2() {
+        let input = read_puzzle("day12.example1");
+        assert_eq!(36, part2(input));
+    }
+
+    #[test]
+    fn example2_day12_part2() {
+        let input = read_puzzle("day12.example2");
+        assert_eq!(103, part2(input));
+    }
+
+    #[test]
+    fn example3_day12_part2() {
+        let input = read_puzzle("day12.example3");
+        assert_eq!(3059, part2(input));
+    }
+
+    #[test]
+    fn exec_day12_part2() {
+        let input = read_puzzle("day12.txt");
+        println!("Day 12 Part 2 - {}", part1(input));
     }
 }
