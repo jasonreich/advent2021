292bad3b9ceb4efac759a06041a6bf696b82d0be
Jason Reich <jason@swiftorific.co.uk>
2021-12-12 07:38:24 +0000

    Parser

--- src/day12.rs
+++ src/day12.rs
@@ -1,4 +1,30 @@
+use multimap::MultiMap;
+use std::iter::FromIterator;
+
+use crate::util::read_lines;
+
+type Puzzle = MultiMap<String, String>;
+
+pub fn read_puzzle(file: &str) -> Puzzle {
+    MultiMap::from_iter(
+        read_lines(file, |line| {
+            let mut components = line.split('-');
+            Some((
+                components.next()?.to_string(),
+                components.next()?.to_string(),
+            ))
+        })
+        .unwrap(),
+    )
+}
+
 #[cfg(test)]
 mod test {
-  
+    use super::*;
+
+    #[test]
+    fn test_read_puzzle() {
+        let input = read_puzzle("day12.example1");
+        assert_eq!(2, input.get_vec("start").unwrap().len());
+    }
 }
