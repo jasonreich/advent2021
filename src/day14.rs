abc6491f545dab0ac3bf43ba5ec0377d1b1d405f
Jason Reich <jason.reich@anaplan.com>
2021-12-14 08:39:31 +0000

    Day 14 Part 2

--- src/day14.rs
+++ src/day14.rs
@@ -1,10 +1,12 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 
 use itertools::{Itertools, MinMaxResult};
 
 use crate::util::read_file;
 
-type Substititions = HashMap<(char, char), char>;
+type Bigram = (char, char);
+type Substititions = HashMap<Bigram, char>;
 
 pub struct Puzzle {
     template: String,
@@ -57,6 +59,48 @@ pub fn part1(input: Puzzle) -> usize {
     }
 }
 
+pub fn increase<K: Eq+Hash+Copy>(m: &mut HashMap<K, usize>, k: &K, i: usize) {
+    let old_value = *m.get(k).unwrap_or(&0);
+    m.insert(*k, old_value + i);
+}
+
+pub fn part2(input: &Puzzle, steps: u32) -> usize {
+    let mut bigram_counts: HashMap<Bigram, usize> = HashMap::new();
+
+    // Initialise
+    let template: Vec<char> = input.template.chars().collect();
+    for i in 0..(template.len() - 1) {
+        let bigram = (template[i], template[i+1]);
+        increase(&mut bigram_counts, &bigram, 1);
+    }
+
+    for _ in 0..steps {
+        let mut new_counts: HashMap<Bigram, usize> = HashMap::new();
+        for (bigram, count) in bigram_counts {
+            if let Some(c) = input.substitutions.get(&bigram) {
+                let (a, b) = bigram;
+                increase(&mut new_counts, &(a, *c), count);
+                increase(&mut new_counts, &(*c, b), count);
+            } else {
+                increase(&mut new_counts, &bigram, count);
+            }
+        }
+        bigram_counts = new_counts;
+    }
+
+    let mut counts: HashMap<char, usize> = HashMap::new();
+    for ((a, _), count) in bigram_counts {
+        increase(&mut counts, &a, count);
+    }
+    increase(&mut counts, template.last().unwrap(), 1);
+
+    if let MinMaxResult::MinMax(lower, upper) = counts.values().minmax() {
+        upper - lower
+    } else {
+        unimplemented!()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -85,5 +129,19 @@ mod test {
     fn exec_day14_part1() {
         let input = parse_puzzle("day14.txt");
         println!("Day 14 Part 1 - {}", part1(input));
+    }
+
+    #[test]
+    fn example_day14_part2() {
+        let input = parse_puzzle("day14.example");
+        assert_eq!(1588, part2(&input, 10));
+        assert_eq!(2188189693529, part2(&input, 40));
+    }
+
+    #[test]
+    fn exec_day14_part2() {
+        let input = parse_puzzle("day14.txt");
+        println!("Day 14 Part 1 - {}", part2(&input, 10));
+        println!("Day 14 Part 2 - {}", part2(&input, 40));
     }
 }
