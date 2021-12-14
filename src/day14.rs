8fe02a591b11e582bb99e618607a07d85e28f08e
Jason Reich <jason.reich@anaplan.com>
2021-12-14 08:02:06 +0000

    Day 14 Part 1

--- src/day14.rs
+++ src/day14.rs
@@ -1,10 +1,14 @@
 use std::collections::HashMap;
 
+use itertools::{Itertools, MinMaxResult};
+
 use crate::util::read_file;
+
+type Substititions = HashMap<(char, char), char>;
 
 pub struct Puzzle {
     template: String,
-    substitutions: HashMap<(char, char), char>,
+    substitutions: Substititions,
 }
 
 pub fn parse_puzzle(file: &str) -> Puzzle {
@@ -24,6 +28,35 @@ pub fn parse_puzzle(file: &str) -> Puzzle {
     Puzzle { template, substitutions }
 }
 
+pub fn step(template: String, substitutions: &Substititions) -> String {
+    let input: Vec<char> = template.chars().collect();
+    let mut output: Vec<char> = Vec::new();
+
+    for i in 0..(input.len() - 1) {
+        output.push(input[i]);
+        if let Some(c) = substitutions.get(&(input[i], input[i + 1])) {
+            output.push(*c);
+        }
+    }
+    output.push(*input.last().unwrap());
+
+    output.iter().collect()
+}
+
+pub fn part1(input: Puzzle) -> usize {
+    let mut template = input.template;
+    for _ in 0..10 {
+        template = step(template, &input.substitutions)
+    }
+
+    let output = template.chars().counts();
+    if let MinMaxResult::MinMax(lower, upper) = output.values().minmax() {
+        upper - lower
+    } else {
+        unimplemented!()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -34,5 +67,23 @@ mod test {
         assert_eq!(4, puzzle.template.len());
         assert_eq!(16, puzzle.substitutions.len());
         assert_eq!('B', puzzle.substitutions[&('C', 'H')])
+    }
+
+    #[test]
+    fn test_step() {
+        let puzzle = parse_puzzle("day14.example");
+        assert_eq!("NCNBCHB", step(puzzle.template, &puzzle.substitutions));
+    }
+
+    #[test]
+    fn example_day14_part1() {
+        let input = parse_puzzle("day14.example");
+        assert_eq!(1588, part1(input));
+    }
+
+    #[test]
+    fn exec_day14_part1() {
+        let input = parse_puzzle("day14.txt");
+        println!("Day 14 Part 1 - {}", part1(input));
     }
 }
